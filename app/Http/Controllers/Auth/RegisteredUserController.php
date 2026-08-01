a9e0e9a5c775ce8c8aff62ca605735017d10c6fb
mueblegroup <mueblegroup@gmail.com>
2026-08-01 16:40:53 +0800

    Route new accounts into client portal

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -37,6 +37,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect()->route('onboarding.company.create');
+        return redirect()->route('portal.dashboard');
     }
 }
