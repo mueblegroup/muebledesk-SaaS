9c8067a436b63c30c420dccc64b25074378a6096
mueblegroup <mueblegroup@gmail.com>
2026-08-01 16:54:52 +0800

    Fix HTTPS proxy redirect loop

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -8,21 +8,24 @@ use Illuminate\Http\Request;
 class TrustProxies extends Middleware
 {
     /**
-     * The trusted proxies for this application.
+     * Trust the reverse proxy in front of the Laravel application.
+     *
+     * Hestia/Nginx and Cloudflare terminate HTTPS before forwarding the
+     * request to PHP. Trusting forwarded headers allows Laravel to retain
+     * the original HTTPS scheme and host when generating redirects.
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
      *
      * @var int
      */
-    protected $headers =
-        Request::HEADER_X_FORWARDED_FOR |
-        Request::HEADER_X_FORWARDED_HOST |
-        Request::HEADER_X_FORWARDED_PORT |
-        Request::HEADER_X_FORWARDED_PROTO |
-        Request::HEADER_X_FORWARDED_AWS_ELB;
+    protected $headers = Request::HEADER_X_FORWARDED_FOR
+        | Request::HEADER_X_FORWARDED_HOST
+        | Request::HEADER_X_FORWARDED_PORT
+        | Request::HEADER_X_FORWARDED_PROTO
+        | Request::HEADER_X_FORWARDED_AWS_ELB;
 }
