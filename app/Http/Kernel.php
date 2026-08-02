f2a1bcb23a4d0cc6ab5c574fe44ce764c8d78707
mueblegroup <mueblegroup@gmail.com>
2026-08-02 16:35:28 +0800

    Fix verified middleware alias

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -47,7 +47,7 @@ class Kernel extends HttpKernel
         'precognitive' => \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'role' => \App\Http\Middleware\RoleMiddleware::class,
         '2fa' => \App\Http\Middleware\EnsureTwoFactorIsVerified::class,
         'api.key' => \App\Http\Middleware\AuthenticateApiKey::class,
