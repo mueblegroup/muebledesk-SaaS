5ee3812fb82aa1ae22aaa69014c03ee00c67f2b3
mueblegroup <mueblegroup@gmail.com>
2026-08-02 15:34:55 +0800

    Scope quotations to company tenants

--- app/Models/Quotation.php
+++ app/Models/Quotation.php
@@ -2,14 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\Concerns\BelongsToCompany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Quotation extends Model
 {
-    use HasFactory;
+    use HasFactory, BelongsToCompany;
 
     protected $fillable = [
+        'company_id',
         'client_id',
         'quote_number',
         'date',
