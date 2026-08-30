be5036730eadc2ca9d1a86dc0b524b3328242673
mueblegroup <mueblegroup@gmail.com>
2026-08-30 15:32:54 +0800

    Add registration locale options

---
+++ config/registration.php
@@ -0,0 +1,68 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Registration locale options
+    |--------------------------------------------------------------------------
+    |
+    | Keep the ISO country code separate from the international dial prefix.
+    | Registration stores phone numbers in E.164-like form (+<dial><number>)
+    | while country_code remains the two-letter ISO code used elsewhere.
+    |
+    */
+    'countries' => [
+        'MY' => ['name' => 'Malaysia', 'dial' => '+60'],
+        'SG' => ['name' => 'Singapore', 'dial' => '+65'],
+        'ID' => ['name' => 'Indonesia', 'dial' => '+62'],
+        'TH' => ['name' => 'Thailand', 'dial' => '+66'],
+        'BN' => ['name' => 'Brunei', 'dial' => '+673'],
+        'PH' => ['name' => 'Philippines', 'dial' => '+63'],
+        'VN' => ['name' => 'Vietnam', 'dial' => '+84'],
+        'KH' => ['name' => 'Cambodia', 'dial' => '+855'],
+        'LA' => ['name' => 'Laos', 'dial' => '+856'],
+        'MM' => ['name' => 'Myanmar', 'dial' => '+95'],
+        'IN' => ['name' => 'India', 'dial' => '+91'],
+        'CN' => ['name' => 'China', 'dial' => '+86'],
+        'HK' => ['name' => 'Hong Kong', 'dial' => '+852'],
+        'MO' => ['name' => 'Macao', 'dial' => '+853'],
+        'TW' => ['name' => 'Taiwan', 'dial' => '+886'],
+        'JP' => ['name' => 'Japan', 'dial' => '+81'],
+        'KR' => ['name' => 'South Korea', 'dial' => '+82'],
+        'AU' => ['name' => 'Australia', 'dial' => '+61'],
+        'NZ' => ['name' => 'New Zealand', 'dial' => '+64'],
+        'AE' => ['name' => 'United Arab Emirates', 'dial' => '+971'],
+        'SA' => ['name' => 'Saudi Arabia', 'dial' => '+966'],
+        'QA' => ['name' => 'Qatar', 'dial' => '+974'],
+        'BH' => ['name' => 'Bahrain', 'dial' => '+973'],
+        'KW' => ['name' => 'Kuwait', 'dial' => '+965'],
+        'OM' => ['name' => 'Oman', 'dial' => '+968'],
+        'GB' => ['name' => 'United Kingdom', 'dial' => '+44'],
+        'IE' => ['name' => 'Ireland', 'dial' => '+353'],
+        'FR' => ['name' => 'France', 'dial' => '+33'],
+        'DE' => ['name' => 'Germany', 'dial' => '+49'],
+        'IT' => ['name' => 'Italy', 'dial' => '+39'],
+        'ES' => ['name' => 'Spain', 'dial' => '+34'],
+        'NL' => ['name' => 'Netherlands', 'dial' => '+31'],
+        'BE' => ['name' => 'Belgium', 'dial' => '+32'],
+        'CH' => ['name' => 'Switzerland', 'dial' => '+41'],
+        'AT' => ['name' => 'Austria', 'dial' => '+43'],
+        'SE' => ['name' => 'Sweden', 'dial' => '+46'],
+        'NO' => ['name' => 'Norway', 'dial' => '+47'],
+        'DK' => ['name' => 'Denmark', 'dial' => '+45'],
+        'FI' => ['name' => 'Finland', 'dial' => '+358'],
+        'PL' => ['name' => 'Poland', 'dial' => '+48'],
+        'PT' => ['name' => 'Portugal', 'dial' => '+351'],
+        'GR' => ['name' => 'Greece', 'dial' => '+30'],
+        'TR' => ['name' => 'Türkiye', 'dial' => '+90'],
+        'US' => ['name' => 'United States', 'dial' => '+1'],
+        'CA' => ['name' => 'Canada', 'dial' => '+1'],
+        'MX' => ['name' => 'Mexico', 'dial' => '+52'],
+        'BR' => ['name' => 'Brazil', 'dial' => '+55'],
+        'AR' => ['name' => 'Argentina', 'dial' => '+54'],
+        'ZA' => ['name' => 'South Africa', 'dial' => '+27'],
+        'EG' => ['name' => 'Egypt', 'dial' => '+20'],
+        'NG' => ['name' => 'Nigeria', 'dial' => '+234'],
+        'KE' => ['name' => 'Kenya', 'dial' => '+254'],
+    ],
+];
